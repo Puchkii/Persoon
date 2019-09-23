d87ed93bc7a851f84148b950831c745ecd23e26f
Kevin <kevinblank72@gmail.com>
2019-09-23 18:55:43 +0200

    Database is partially working with java, we still need to have a way to insert full user info. Userlist is coming in the next update.

--- src/Mysqlcon.java
+++ src/Mysqlcon.java
@@ -2,28 +2,45 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.List;
 
 public class Mysqlcon {
 
 
 
-	static Connection connection = null;
-	static String databaseName = "persoon";
-	static String url = "jdbc:mysql://localhost:3306/" +databaseName;
+	Connection connection = null;
+	static String databaseName = "persoondb";
+	static String url = "jdbc:mysql://localhost:3306/" +databaseName+ "?serverTimezone=CET";
 
-	static String username = "`puchkii`";
+	static String username = "puchkii";
 	static String password = "4>uF2=JC4%xa->C";
 
-	public static void main(String args[]) throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-		connection = DriverManager.getConnection(url,username,password);
-		PreparedStatement ps = connection.prepareStatement("INSERT INTO `persoon`.`voornaam` (`persoonFirstname`) VALUES ('setVoornaam');");//deze moet nog nodig worden gefixt
 
-		int status = ps.executeUpdate();
 
-		if (status != 0){
-			System.out.println("Database was connected");
-			System.out.println("Record was inserted");
+	public void init(){
+		try {
+			connection = DriverManager.getConnection(url,username,password);
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+	}
 
+	public void insertPerson(Persoon persoon) {
+		try {
+			PreparedStatement ps = connection.prepareStatement("INSERT INTO `personen` (`firstname`) VALUES ('"+persoon.getVoornaam()+ "')");
+			int status = ps.executeUpdate();
+
+			if (status != 0){
+				System.out.println("Database was connected");
+				System.out.println("Record was inserted");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Persoon> getAll(){
+
+		return null;
 	}
 }
--- src/Run.java
+++ src/Run.java
@@ -1,55 +1,78 @@
-public class Run extends Mysqlcon{
+import javax.swing.JFrame;
+
+import java.awt.GraphicsConfiguration;
+
+import java.util.List;
+
+public class Run{
+
+	static GraphicsConfiguration gc;
 	public static void main(String[] args){
+		JFrame myFrame = new JFrame(gc);
+		myFrame.setTitle("Userlist");
+		myFrame.setVisible(true);
+		myFrame.setSize(300, 500);
+		myFrame.setBounds(100, 200, 1200, 600);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		//gegevens word hier gekoppeld aan Persoon bij naam bijv. kevin
+		Mysqlcon mysqlcon = new Mysqlcon();
+		mysqlcon.init();
 
 		Persoon kevin = new Persoon();
 		kevin.setVoornaam("Kevin");
 		kevin.setAchternaam("Blank");
 		kevin.setGeboorteDatum("11-01-1999");
 		System.out.println(kevin);
+//		mysqlcon.insertPerson(kevin);
 
 		Persoon sidian = new Persoon();
 		sidian.setVoornaam("Sidian");
 		sidian.setAchternaam("Koné");
 		sidian.setGeboorteDatum("25-9-2001");
 		System.out.println(sidian);
+//		mysqlcon.insertPerson(sidian);
 
-		Persoon edward = new Persoon();
-		edward.setVoornaam("Edward");
-		edward.setAchternaam("Hess");
-		edward.setGeboorteDatum("31-10-1958");
-		System.out.println(edward);
+//		List<Persoon> alles = mysqlcon.getAll();
+//		for (Persoon p : alles) {
+//			System.out.println(p.getVoornaam());
+//		}
 
-		Persoon anouk = new Persoon();
-		anouk.setVoornaam("Anouk");
-		anouk.setAchternaam("Gemmer");
-		anouk.setGeboorteDatum("06-02-2002");
-		System.out.println(anouk);
-
-		Persoon jozef = new Persoon();
-		jozef.setVoornaam("Jozef");
-		jozef.setAchternaam("Tromp");
-		jozef.setGeboorteDatum("04-09-1999");
-		System.out.println(jozef);
-
-		Persoon lars = new Persoon();
-		lars.setVoornaam("Lars");
-		lars.setAchternaam("Stuursma");
-		lars.setGeboorteDatum("25-05-2001");
-		System.out.println(lars);
-
-		Persoon dylan = new Persoon();
-		dylan.setVoornaam("Dylan");
-		dylan.setAchternaam("Spin");
-		dylan.setGeboorteDatum("09-07-2002");
-		System.out.println(dylan);
-
-		Persoon angela = new Persoon();
-		angela.setVoornaam("Angela");
-		angela.setAchternaam("Blank");
-		angela.setGeboorteDatum("15-10-1967");
-		System.out.println(angela);
-
+//		Persoon edward = new Persoon();
+//		edward.setVoornaam("Edward");
+//		edward.setAchternaam("Hess");
+//		edward.setGeboorteDatum("31-10-1958");
+//		System.out.println(edward);
+//
+//		Persoon anouk = new Persoon();
+//		anouk.setVoornaam("Anouk");
+//		anouk.setAchternaam("Gemmer");
+//		anouk.setGeboorteDatum("06-02-2002");
+//		System.out.println(anouk);
+//
+//		Persoon jozef = new Persoon();
+//		jozef.setVoornaam("Jozef");
+//		jozef.setAchternaam("Tromp");
+//		jozef.setGeboorteDatum("04-09-1999");
+//		System.out.println(jozef);
+//
+//		Persoon lars = new Persoon();
+//		lars.setVoornaam("Lars");
+//		lars.setAchternaam("Stuursma");
+//		lars.setGeboorteDatum("25-05-2001");
+//		System.out.println(lars);
+//
+//		Persoon dylan = new Persoon();
+//		dylan.setVoornaam("Dylan");
+//		dylan.setAchternaam("Spin");
+//		dylan.setGeboorteDatum("09-07-2002");
+//		System.out.println(dylan);
+//
+//		Persoon angela = new Persoon();
+//		angela.setVoornaam("Angela");
+//		angela.setAchternaam("Blank");
+//		angela.setGeboorteDatum("15-10-1967");
+//		System.out.println(angela);
+//
 	}
 }
