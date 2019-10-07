7a694fcbd11b9ba5030b2e242eb109909165c2c0
Kevin <kevinblank72@gmail.com>
2019-10-07 11:04:50 +0200

    PersonTableModel class //WIP

--- src/util/PersonTableModel.java
+++ src/util/PersonTableModel.java
@@ -1,79 +1,87 @@
 package util;
 
 import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersonTableModel implements TableModel {
+public class PersonTableModel extends DefaultTableModel {
 	List<Person> personen = new ArrayList<Person>();
 
 	public PersonTableModel(){
-		personen.add(new Person("Firstname", "Lastname", "6"));
-		personen.add(new Person("Joep", "Meloen","9"));
-		personen.add(new Person("Piet", "Verdriet","6"));
+		columnIdentifiers.add("Voornaam");
+		columnIdentifiers.add("Voornaam");
+		columnIdentifiers.add("Voornaam");
 
+		personen.add(new Person("Firstname", "Lastname",6));
+		personen.add(new Person("Joep", "Meloen",9));
+		personen.add(new Person("Piet", "Verdriet",6));
+
+		String[] dummy = {personen.get(0).getFirstName(), personen.get(0).getLastName(), Integer.toString(personen.get(0).getDaysAlive())};
+		addRow(dummy);
 	}
 
 	public List<Person> getPersonen() {
 		return personen;
 	}
 
-	@Override
-	public int getRowCount() {
-		return personen.size();
-	}
-
-	@Override
-	public int getColumnCount() {
-		return 3;
-	}
-
-	@Override
-	public String getColumnName(int i) {
-		switch (i){
-			case 0: return "Voornaam";
-			case 1: return "Achternaam";
-			case 2: return "Geboortedatum";
-			default: return "Pizza is lekker";
-		}
-}
-
-	@Override
-	public Class<?> getColumnClass(int i) {
-		return String.class;
-	}
-
-	@Override
-	public boolean isCellEditable(int i, int i1) {
-		return false;
-	}
-
-	@Override
-	public Object getValueAt(int i, int i1) {
-		Person p = personen.get(i);
-		switch (i1) {
-			case 0:
-				return p.getFirstName();
-			case 1:
-				return p.getLastName();
-
-			default: return "";
-		}
-	}
-
-	@Override
-	public void setValueAt(Object o, int i, int i1) {
-
-	}
-
-	@Override
-	public void addTableModelListener(TableModelListener tableModelListener) {
-
-	}
-
-	@Override
-	public void removeTableModelListener(TableModelListener tableModelListener) {
-
-	}
+//	@Override
+//	public int getRowCount() {
+//		return personen.size();
+//	}
+//
+//	@Override
+//	public int getColumnCount() {
+//		return 3;
+//	}
+//
+//	@Override
+//	public String getColumnName(int i) {
+//		switch (i){
+//			case 0: return "Voornaam";
+//			case 1: return "Achternaam";
+//			case 2: return "Geboortedatum";
+//			default: return "Pizza is lekker";
+//		}
+//	}
+//
+//	@Override
+//	public Class<?> getColumnClass(int i) {
+//		return String.class;
+//	}
+//
+//	@Override
+//	public boolean isCellEditable(int i, int i1) {
+//		return false;
+//	}
+//
+//	@Override
+//	public Object getValueAt(int i, int i1) {
+//		Person p = personen.get(i);
+//		switch (i1) {
+//			case 0:
+//				return p.getFirstName();
+//			case 1:
+//				return p.getLastName();
+//			case 2:
+//				return p.getDaysAlive();
+//			default: return "";
+//		}
+//	}
+//
+//	@Override
+//	public void setValueAt(Object o, int i, int i1) {
+//
+//	}
+//
+//	@Override
+//	public void addTableModelListener(TableModelListener tableModelListener) {
+//
+//	}
+//
+//	@Override
+//	public void removeTableModelListener(TableModelListener tableModelListener) {
+//
+//	}
 }
